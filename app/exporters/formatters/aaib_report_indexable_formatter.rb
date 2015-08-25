42a62591f474b15f51348109cae73915044d9207
tijmen <tijmen@gmail.com>
2015-08-25 22:22:14 +0100

    Send aircraft_category_name and report_type_name to rummager

--- app/exporters/formatters/aaib_report_indexable_formatter.rb
+++ app/exporters/formatters/aaib_report_indexable_formatter.rb
@@ -9,12 +9,18 @@ private
   def extra_attributes
     {
       aircraft_category: entity.aircraft_category,
+      aircraft_category_name: expand_value(:aircraft_category, entity.aircraft_category),
       report_type: entity.report_type,
+      report_type_name: expand_value(:report_type, entity.report_type),
       date_of_occurrence: entity.date_of_occurrence,
       location: entity.location,
       aircraft_type: entity.aircraft_type,
       registration: entity.registration,
     }
+  end
+
+  def expand_value(key, value)
+    SpecialistPublisherWiring.get(:aaib_report_finder_schema).humanized_facet_value(key, value)
   end
 
   def organisation_slugs
