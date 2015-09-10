ee0aed560e086531615daf082efd93a0e17d617b
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-09-10 10:42:33 +0100

    Move logic of deciding whether to publish finder out of call method

--- lib/publishing_api_finder_publisher.rb
+++ lib/publishing_api_finder_publisher.rb
@@ -9,23 +9,27 @@ class PublishingApiFinderPublisher
   end
 
   def call
-    finders.map do |finder|
-      if finder[:metadata].has_key?("content_id") && !preview_only?(finder)
-        publish(finder)
-      elsif preview_only?(finder)
-        if preview_domain_or_not_production?
-          publish(finder)
-        else
-          puts "didn't publish #{finder[:metadata]["name"]} because it is preview_only" if @log
-        end
-      else
-        puts "didn't publish #{finder[:metadata]["name"]} because it doesn't have a content_id" if @log
-      end
+    finders.each do |finder|
+      publish_if_should(finder)
     end
   end
 
 private
   attr_reader :finders
+
+  def publish_if_should(finder)
+    if finder[:metadata].has_key?("content_id") && !preview_only?(finder)
+      publish(finder)
+    elsif preview_only?(finder)
+      if preview_domain_or_not_production?
+        publish(finder)
+      else
+        puts "didn't publish #{finder[:metadata]["name"]} because it is preview_only" if @log
+      end
+    else
+      puts "didn't publish #{finder[:metadata]["name"]} because it doesn't have a content_id" if @log
+    end
+  end
 
   def publish(finder)
     export_finder(finder)
