2e25ed48b0703bf64e4e1dbc2c7a388d34afedd1
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-11-16 16:13:22 +0000

    Add Document and CMA Case model
    The CMA Case model inherits from Document. The `FORMAT_SPECIFIC_FIELDS`
    is used to set up the methods for the extra fields for each format.

--- app/models/cma_case.rb
+++ app/models/cma_case.rb
@@ -1,4 +1,5 @@
 class CmaCase < Document
+<<<<<<< HEAD
 
   validates :opened_date, allow_blank: true, date: true
   validates :market_sector, presence: true
@@ -6,6 +7,8 @@ class CmaCase < Document
   validates :case_state, presence: true
   validates :closed_date, allow_blank: true, date: true
 
+=======
+>>>>>>> Add Document and CMA Case model
   FORMAT_SPECIFIC_FIELDS = [
     :opened_date,
     :closed_date,
@@ -28,4 +31,12 @@ class CmaCase < Document
   def public_path
     "/cma-cases"
   end
+<<<<<<< HEAD
+=======
+
+  def organisations
+    # Org name: Competition And Markets Authority
+    ["957eb4ec-089b-4f71-ba2a-dc69ac8919ea"]
+  end
+>>>>>>> Add Document and CMA Case model
 end
