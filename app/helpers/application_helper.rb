a561c528d8414be06e669b3ef5171adc9c974007
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-11-16 16:29:18 +0000

    Add views and assets for making new Documents
    This commit includes all the views and assets needed to make new
    Documents. The metadata fields are included by finding out what the
    current_format is. Most of this is copied straight from the existing
    specialist publisher.
    
    This commit also adds jQuery and select2-rails.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,3 @@
 module ApplicationHelper
 
-  def facet_options(form, facet)
-    form.object.facet_options(facet)
-  end
-
-  def published_document_path(document)
-    Plek.current.find("website-root") + document.base_path
-  end
 end
